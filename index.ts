3e42c69f38067f66858eb71bd7699be81ce1baaf
dg12988 <doug.goldberg@me.com>
2021-08-10 02:39:29 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
+import { Observable, timer } from "rxjs";
 
+console.log('App Started');
+
+const timer$ = new Observable<number>(subscriber => {
+  const timeoutID = setTimeout(() => {
+    console.log('Timeout');
+    subscriber.next(0);
+    subscriber.complete();
+  }, 2000);
+
+  return () => clearTimeout(timeoutID);
+});
+
+const subscription = timer$.subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed!')
+});
+
+setTimeout(() => {
+  subscription.unsubscribe();
+  console.log('unsubscribe');
+}, 1000);
